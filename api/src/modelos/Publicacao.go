fecba1c9ba8ca85fc920333e221f835e2caa1a12
filgueirasjulio <julio_filgueiras@live.com>
2022-06-01 21:58:59 -0300

    feat(api): validação de publicação (store)

--- api/src/modelos/Publicacao.go
+++ api/src/modelos/Publicacao.go
@@ -1,6 +1,8 @@
 package modelos
 
 import (
+	"errors"
+	"strings"
 	"time"
 )
 
@@ -13,4 +15,32 @@ type Publicacao struct {
 	AutorNick string    `json:"autorNick,omitempty"`
 	Curtidas  uint64    `json:"curtidas"`
 	CriadaEm  time.Time `json:"criadaEm,omitempty"`
+}
+
+func (publicacao *Publicacao) Preparar() error {
+	if erro := publicacao.validar(); erro != nil {
+		return erro
+	}
+
+	publicacao.formatar()
+	return nil
+}
+
+func (publicacao *Publicacao) validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("O título é obrigatório e não pode estar em branco")
+	} 
+
+	if publicacao.Conteudo == "" {
+		return errors.New("O conteúdo é obrigatório e não pode estar em branco")
+	} 
+
+	return nil
+}
+
+func (publicacao *Publicacao) formatar() error {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
+
+	return nil
 }
